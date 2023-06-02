08b5fcdbee17cb5928429d371ce6da70c4d6492f
Zebbeni <andrew.albers@gmail.com>
2023-06-02 10:53:26 -0600

    style browser like other components

--- controls/menu/model.go
+++ controls/menu/model.go
@@ -2,6 +2,9 @@ package menu
 
 import (
 	"github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/lipgloss"
+
+	"github.com/Zebbeni/ansizalizer/style"
 )
 
 func New(items []list.Item) list.Model {
@@ -22,5 +25,20 @@ func NewDelegate() list.DefaultDelegate {
 	delegate := list.NewDefaultDelegate()
 	delegate.SetSpacing(0)
 	delegate.ShowDescription = false
+	delegate.Styles = ItemStyles()
 	return delegate
+}
+
+func ItemStyles() (s list.DefaultItemStyles) {
+	s.NormalTitle = style.DimmedTitle.Copy().Padding(0, 1, 0, 2)
+	s.NormalDesc = style.DimmedParagraph.Copy().MaxHeight(1).Padding(0, 0, 0, 2)
+
+	s.SelectedTitle = style.SelectedTitle.Copy().Padding(0, 1, 0, 1).
+		Border(lipgloss.NormalBorder(), false, false, false, true).
+		BorderForeground(style.SelectedColor1)
+
+	s.DimmedTitle = style.DimmedTitle.Copy().Padding(0, 1, 0, 0)
+	s.DimmedDesc = style.DimmedParagraph.Copy().MaxHeight(1).Padding(0, 0, 0, 2)
+
+	return s
 }
