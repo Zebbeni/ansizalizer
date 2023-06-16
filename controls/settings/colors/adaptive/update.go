d544974999db1ae4e9f3267000cfc3afdb152105
Zebbeni <andrew.albers@gmail.com>
2023-06-16 10:48:15 -0600

    suppport saving .hex files

--- controls/settings/colors/adaptive/update.go
+++ controls/settings/colors/adaptive/update.go
@@ -1,6 +1,11 @@
 package adaptive
 
 import (
+	"fmt"
+	"image/color"
+	"os"
+	"path/filepath"
+
 	"github.com/charmbracelet/bubbles/key"
 	tea "github.com/charmbracelet/bubbletea"
 
@@ -37,6 +42,8 @@ func (m Model) handleEnter() (Model, tea.Cmd) {
 	case IterForm:
 		m.iterInput.Focus()
 		return m, nil
+	case Save:
+		return m.savePaletteFile()
 	}
 	return m, io.BuildAdaptingCmd()
 }
@@ -65,4 +72,37 @@ func (m Model) handleNav(msg tea.KeyMsg) (Model, tea.Cmd) {
 	}
 
 	return m, cmd
+}
+
+func (m Model) savePaletteFile() (Model, tea.Cmd) {
+	filename := fmt.Sprintf("%s.hex", m.palette.Name())
+
+	f, err := os.Create(filename)
+
+	if err != nil {
+		return m, io.BuildDisplayCmd("error saving palette file")
+	}
+
+	defer f.Close()
+
+	var hexStrings string
+
+	for _, c := range m.palette.Colors() {
+		hexStrings += hexColor(c) + "\n"
+
+		if err != nil {
+			return m, io.BuildDisplayCmd("error writing to palette file")
+		}
+	}
+
+	_, err = f.WriteString(hexStrings)
+
+	dir, _ := os.Getwd()
+	msg := fmt.Sprintf("saved %s in %s", filename, filepath.Base(dir))
+	return m, io.BuildDisplayCmd(msg)
+}
+
+func hexColor(c color.Color) string {
+	rgba := color.RGBAModel.Convert(c).(color.RGBA)
+	return fmt.Sprintf("%.2x%.2x%.2x", rgba.R, rgba.G, rgba.B)
 }
