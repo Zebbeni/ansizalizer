f59734c6ed9ac7cd59601b9032cb2218bd9c1a7d
EvilJordan <jrh@jordanholberg.com>
2025-12-06 15:37:40 -0800

    simple concatentation of output string, keeping all blank space to not affect view size

--- app/process/common.go
+++ app/process/common.go
@@ -1,8 +1,6 @@
 package process
 
 import (
-	"strings"
-	"bufio"
 	"github.com/charmbracelet/lipgloss"
 )
 
@@ -11,17 +9,5 @@ const (
 )
 
 func (m Renderer) outputStrings(rows ...string) (string) {
-	content := ""
-	if m.Settings.Alpha.ShouldOutputAlpha() {
-		contentAlpha := lipgloss.JoinVertical(lipgloss.Left, rows...)
-		// iterate through the return of JoinVertical, separating by lines, trimming our AlphaPlaceholder from the right, and then recombining
-		reader := strings.NewReader(contentAlpha)
-		scanner := bufio.NewScanner(reader)
-		for scanner.Scan() {
-			content += strings.TrimRight(scanner.Text(), " ") + "\n"
-		}
-	} else {
-		content += lipgloss.JoinVertical(lipgloss.Left, rows...)
-	}
-	return content
+	return lipgloss.JoinVertical(lipgloss.Left, rows...)
 }
