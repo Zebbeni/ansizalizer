1125f0dd9eb1cc5ed5f89392f993a51018140064
EvilJordan <jrh@jordanholberg.com>
2025-12-06 16:15:48 -0800

    allow .gif images to be loaded

--- global/file.go
+++ global/file.go
@@ -1,5 +1,5 @@
 package global
 
 var (
-	ImgExtensions = map[string]bool{".png": true, ".jpg": true, ".jpeg": true}
+	ImgExtensions = map[string]bool{".png": true, ".jpg": true, ".jpeg": true, ".gif": true}
 )
